83443e4a7be35e767a378a2e8e43466d57b4cdbf
andrewackerman <andrew.ackie@gmail.com>
2019-06-23 19:49:07 -0600

    Removed packages folder, moved project files into root

--- lib/src/event_emitter.dart
+++ lib/src/event_emitter.dart
@@ -1,34 +1,41 @@
 import 'dart:async';
 
+import 'package:meta/meta.dart';
 import 'package:nbtb/src/event_subscriber.dart';
 import 'package:nbtb/src/typedefs.dart';
+import 'package:rxdart/rxdart.dart';
 
-class EventEmitter<E> {
-  final StreamController<E> controller;
+abstract class IEventEmitter<E> {
+  StreamController<E> get controller;
 
-  EventEmitter()
-      : controller = StreamController<E>.broadcast();
-
-  EventEmitter.withStream(Stream<E> stream)
-      : controller = StreamController<E>.broadcast() {
-    ArgumentError.checkNotNull(stream);
-    controller.addStream(stream);
+  EventSubscriber<E> createSubscription({
+    OnStreamEvent<E> onEvent,
+    OnStreamError onError,
+    OnStreamDone onDone,
+  }) {
+    return EventSubscriber<E>(onEvent: onEvent, onError: onError, onDone: onDone)
+      ..controller.addStream(this.controller.stream);
   }
 
   void emit(E event) {
     controller.add(event);
   }
 
-  EventSubscriber<E> createSubscription({
-      OnStreamEvent<E> onEvent,
-      OnStreamError onError,
-      OnStreamDone onDone,
-    }) {
-    return EventSubscriber<E>(onEvent: onEvent, onError: onError, onDone: onDone)
-      ..controller.addStream(this.controller.stream);
-  }
-
+  @mustCallSuper
   void close() {
     controller.close();
+  }
+}
+
+class EventEmitter<E> extends IEventEmitter<E> {
+  final PublishSubject<E> controller;
+
+  EventEmitter()
+      : controller = PublishSubject<E>();
+
+  EventEmitter.withStream(Stream<E> stream)
+      : controller = PublishSubject<E>() {
+    ArgumentError.checkNotNull(stream);
+    controller.addStream(stream);
   }
 }
--- lib/src/event_subscriber.dart
+++ lib/src/event_subscriber.dart
@@ -1,12 +1,31 @@
 import 'dart:async';
 
+import 'package:meta/meta.dart';
 import 'package:nbtb/src/event_emitter.dart';
 import 'package:nbtb/src/event_route_node.dart';
 import 'package:nbtb/src/event_transformer.dart';
 import 'package:nbtb/src/typedefs.dart';
+import 'package:rxdart/rxdart.dart';
 
-class EventSubscriber<E> extends EventRouteNode<E> {
-  final StreamController<E> controller;
+abstract class IEventSubscriber<E> extends EventRouteNode<E> {
+  StreamController<E> get controller;
+
+  void addEmitter(EventEmitter<E> emitter) {
+    controller.addStream(emitter.controller.stream);
+  }
+
+  StreamSubscription<E> listen(onEvent, {onError, onDone}) {
+    return controller.stream.listen(onEvent, onError: onError, onDone: onDone);
+  }
+
+  @mustCallSuper
+  void close() {
+    controller.close();
+  }
+}
+
+class EventSubscriber<E> extends IEventSubscriber<E> {
+  final BehaviorSubject<E> controller;
 
   EventTransformer transformer;
 
@@ -14,31 +33,31 @@ class EventSubscriber<E> extends EventRouteNode<E> {
     OnStreamEvent<E> onEvent,
     OnStreamError onError,
     OnStreamDone onDone,
-  }) : controller = StreamController<E>.broadcast() {
+  }) : controller = BehaviorSubject<E>() {
     assert (onEvent != null || (onError == null && onDone == null));
     if (onEvent != null) {
       listen(onEvent, onError: onError, onDone: onDone);
     }
   }
 
-//  EventSubscriber.seeded(
-//    E initialValue, {
-//      OnStreamEvent<E> onEvent,
-//      OnStreamError onError,
-//      OnStreamDone onDone,
-//  }) : stream = BehaviorSubject<E>.seeded(initialValue) {
-//    assert (onEvent != null || (onError == null && onDone == null));
-//    if (onEvent != null) {
-//      listen(onEvent, onError: onError, onDone: onDone);
-//    }
-//  }
+  EventSubscriber.seeded(
+    E initialValue, {
+      OnStreamEvent<E> onEvent,
+      OnStreamError onError,
+      OnStreamDone onDone,
+  }) : controller = BehaviorSubject<E>.seeded(initialValue) {
+    assert (onEvent != null || (onError == null && onDone == null));
+    if (onEvent != null) {
+      listen(onEvent, onError: onError, onDone: onDone);
+    }
+  }
 
   EventSubscriber.withStream(
     Stream stream, {
       OnStreamEvent<E> onEvent,
       OnStreamError onError,
       OnStreamDone onDone,
-  }) : this.controller = StreamController<E>.broadcast() {
+  }) : this.controller = BehaviorSubject<E>() {
     assert (onEvent != null || (onError == null && onDone == null));
     this.controller.addStream(stream);
     if (onEvent != null) {
@@ -53,15 +72,5 @@ class EventSubscriber<E> extends EventRouteNode<E> {
     );
   }
 
-  void addEmitter(EventEmitter<E> emitter) {
-    controller.addStream(emitter.controller.stream);
-  }
 
-  StreamSubscription<E> listen(onEvent, {onError, onDone}) {
-    return controller.stream.listen(onEvent, onError: onError, onDone: onDone);
-  }
-
-  void close() {
-    controller.close();
-  }
 }
