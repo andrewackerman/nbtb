2950521026b4ef25566c2ba2e6f2d4677017fb30
andrewackerman <andrew.ackie@gmail.com>
2019-06-24 10:33:44 -0600

    Removing event node base class

--- lib/src/event_route_node.dart
+++ lib/src/event_route_node.dart
@@ -4,24 +4,4 @@ import 'package:nbtb/src/event_transformer.dart';
 import 'package:nbtb/src/typedefs.dart';
 
 abstract class EventRouteNode<E> {
-  StreamSubscription<E> listen(
-    OnStreamEvent<E> onEvent, {
-    OnStreamError onError,
-    OnStreamDone onDone,
-  });
-
-  EventRouteNode<T> transform<T>(EventTransformer<E, T> transformer);
-
-  EventRouteNode<T> map<T>(T Function(E event) handler)
-    => transform(EventTransformer.map(handler));
-  EventRouteNode<E> skip(int count)
-    => transform(EventTransformer.skip(count));
-  EventRouteNode<E> skipWhile(bool Function(E data) condition)
-    => transform(EventTransformer.skipWhile(condition));
-  EventRouteNode<E> take(int count)
-    => transform(EventTransformer.take(count));
-  EventRouteNode<E> takeWhile(bool Function(E data) condition)
-    => transform(EventTransformer.takeWhile(condition));
-  EventRouteNode<E> where(bool Function(E data) condition)
-    => transform(EventTransformer.where(condition));
 }
--- lib/src/event_subscriber.dart
+++ lib/src/event_subscriber.dart
@@ -7,7 +7,7 @@ import 'package:nbtb/src/event_transformer.dart';
 import 'package:nbtb/src/typedefs.dart';
 import 'package:rxdart/rxdart.dart';
 
-abstract class IEventSubscriber<E> extends EventRouteNode<E> {
+abstract class IEventSubscriber<E> {
   StreamController<E> get controller;
 
   void addEmitter(EventEmitter<E> emitter) {
@@ -17,6 +17,21 @@ abstract class IEventSubscriber<E> extends EventRouteNode<E> {
   StreamSubscription<E> listen(onEvent, {onError, onDone}) {
     return controller.stream.listen(onEvent, onError: onError, onDone: onDone);
   }
+
+  EventRouteNode<T> transform<T>(EventTransformer<E, T> transformer);
+
+  EventRouteNode<T> map<T>(T Function(E event) handler)
+  => transform(EventTransformer.map(handler));
+  EventRouteNode<E> skip(int count)
+  => transform(EventTransformer.skip(count));
+  EventRouteNode<E> skipWhile(bool Function(E data) condition)
+  => transform(EventTransformer.skipWhile(condition));
+  EventRouteNode<E> take(int count)
+  => transform(EventTransformer.take(count));
+  EventRouteNode<E> takeWhile(bool Function(E data) condition)
+  => transform(EventTransformer.takeWhile(condition));
+  EventRouteNode<E> where(bool Function(E data) condition)
+  => transform(EventTransformer.where(condition));
 
   @mustCallSuper
   void close() {
